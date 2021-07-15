fd46d58ab2ba19b0cb32e8457b9eda3cc5fe44ef
Malcolm H <malcolm.hinds@hotmail.com>
2021-07-15 23:31:23 +0000

    Updated Main.TF on Linux Machine

--- main.tf
+++ main.tf
@@ -40,23 +40,24 @@ resource "aws_security_group" "webtraffic" {
   
   dynamic "ingress" {
     iterator = port
-    for_each - var.ingress
+    for_each = var.ingress
     content {
       from_port = port.value
       to_port = port.value
       protocol = "TCP"
       cidr_blocks = ["0.0.0.0/0"]
+    }
   }
-}
     dynamic "egress" {
     iterator = port
-    for_each - var.egress
+    for_each = var.egress
     content {
       from_port = port.value
       to_port = port.value
       protocol = "TCP"
       cidr_blocks = ["0.0.0.0/0"]
-  }
+   }
+ }
 }
 
 output "PrivateIP" {
