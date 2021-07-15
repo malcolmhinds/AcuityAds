37399a775c040c004f9b12776d2d202e0d5b6afa
malcolmhinds <40967754+malcolmhinds@users.noreply.github.com>
2021-07-15 18:44:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,30 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami = "ami-0db72f413fc1ddb2a"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.webtraffic.name]
 }
 
 resource "aws_eip" "elasticip" {
   instance = aws_instance.ec2.id
 }
 
+resource "aws_security_group" "webtraffic" {
+  name = "Allow HTTPS"
+  
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  engress {
+    from_port = 443
+    to_port = 443
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 output "EIP" {
   value = aws_eip.elasticip.public_ip
 }
-
