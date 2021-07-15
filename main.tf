3dea8e6a91166dd86907e62692a15c94721a66b3
malcolmhinds <40967754+malcolmhinds@users.noreply.github.com>
2021-07-15 18:29:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,19 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+resource "aws_instance" "ec2" {
+  ami = "ami-0db72f413fc1ddb2a"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "elasticip" {
+  instance = aws_instance.ec2.id
+}
+
+output "EIP" {
+  value = aws_eip.elasticip.public_ip
+}
+
 resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.0.0/16"
+   value
 }
