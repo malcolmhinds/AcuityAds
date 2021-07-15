3b5f6e718a9e5de6c4fbdeda120fa1e202513e93
malcolmhinds <40967754+malcolmhinds@users.noreply.github.com>
2021-07-15 19:12:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,33 +2,67 @@ provider "aws" {
   region = "ca-central-1"
 }
 
-resource "aws_instance" "ec2" {
+#Variables for Dynamic Ports
+variable "ingress" {
+  type = list(number)
+  default = [80,443]
+}
+
+variable "egress" {
+  type = list(number)
+  default = [80,443]
+}
+resource "aws_instance" "db" {
   ami = "ami-0db72f413fc1ddb2a"
   instance_type = "t2.micro"
+  tags {
+    Name = "DB Server"
+  }
+  security_groups = [aws_security_group.webtraffic.name]
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0db72f413fc1ddb2a"
+  instance_type = "t2.micro"
+  user_data = file("server-script.sh")
+  tags {
+    Name = "Web Server"
+  }
   security_groups = [aws_security_group.webtraffic.name]
 }
 
 resource "aws_eip" "elasticip" {
-  instance = aws_instance.ec2.id
+  instance = aws_instance.web.id
 }
 
 resource "aws_security_group" "webtraffic" {
-  name = "Allow HTTPS"
+  name = "Allow Web Traffic"
   
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    iterator = port
+    for_each - var.ingress
+    content {
+      from_port = port.value
+      to_port = port.value
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    from_port = 443
-    to_port = 443
-    protocol = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+}
+    dynamic "egress" {
+    iterator = port
+    for_each - var.egress
+    content {
+      from_port = port.value
+      to_port = port.value
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-output "EIP" {
-  value = aws_eip.elasticip.public_ip
+output "PrivateIP" {
+  value = aws_instance.db.private_ip
+}
+  
+output "PublicIP" {
+  value = aws_eip.web_ip.public_ip
 }
