69d91bbf2a3962cfa08ec305f615229aef5bb94e
malcolmhinds <40967754+malcolmhinds@users.noreply.github.com>
2021-07-15 21:30:47 -0400

    Update web.tf

--- web/web.tf
+++ web/web.tf
@@ -1,1 +1,13 @@
+variable "web_name" {
+  type = string
+}
 
+resource "aws_instance" "web" {
+  ami = "ami-0db72f413fc1ddb2a"
+  instance_type = "t2.micro"
+  user_data = file("server-script.sh")
+  tags = {
+    Name = "Web Server"
+  }
+  security_groups = [aws_security_group.webtraffic.name]
+}
