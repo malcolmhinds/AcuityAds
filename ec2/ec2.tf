97aca5e4a1c62204f7e531071b37db3fb8cf2b1d
malcolmhinds <40967754+malcolmhinds@users.noreply.github.com>
2021-07-15 20:51:35 -0400

    Create ec2.tf

---
+++ ec2/ec2.tf
@@ -0,0 +1,11 @@
+variable "ec2name" {
+    type = string
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0db72f413fc1ddb2a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "First EC2 Server"
+  }
+}
