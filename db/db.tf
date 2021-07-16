8e610cc885b09b085c1ee06a23add58d16499ed8
malcolmhinds <40967754+malcolmhinds@users.noreply.github.com>
2021-07-15 21:20:04 -0400

    Update db.tf

--- db/db.tf
+++ db/db.tf
@@ -1,12 +1,12 @@
-variable "ec2name" {
+variable "dbname" {
     type = string
 }
 
-resource "aws_instance" "ec2" {
+resource "aws_instance" "db" {
   ami = "ami-0db72f413fc1ddb2a"
   instance_type = "t2.micro"
   tags = {
-    Name = "var.ec2name"
+    Name = "var.dbname
   }
 }
 
