8f5c583faabbe3017bd1be828c530d155fb78045
malcolmhinds <40967754+malcolmhinds@users.noreply.github.com>
2021-07-15 21:50:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,27 @@ provider "aws" {
     region = "ca-central-1"
 }
 
-module "ec2module" {
-  source = "./ec2"
-  ec2name = "Name From Module"
+module "db" {
+  source = "./db"
+  db_name = "DB Server"
 }
 
-output "moduyle _output" {
-    value = moodule.ec2module.instance_id
+module "web" {
+  source = "./ec2"
+  web_name = "Web Server"
+}
+    
+module "eip" {
+  source = "./eip"
+}
+
+module "sg" {
+  source = "./sg"
+}
+output "PrivateIP" {
+    value = moodule.db.PrivateIP
+}
+
+output "PublicIP" {
+    value = moodule.web.Elastic_IP
 }
