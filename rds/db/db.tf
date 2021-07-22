2cb9bf4936aee8164844105c5ab2ddead928a3dc
malcolmhinds <40967754+malcolmhinds@users.noreply.github.com>
2021-07-21 21:49:14 -0400

    Create db.tf

---
+++ rds/db/db.tf
@@ -0,0 +1,16 @@
+variable "server_names" {
+    type = string
+}
+
+resource "aws_instance" "db" {
+  ami = "ami-0db72f413fc1ddb2a"
+  instance_type = "t2.micro"
+  count = length(var.server_names)
+  tags = {
+    Name = var.server_names[count.index]
+  }
+}
+
+output "PrivateIP" {
+    value = aws_instrance.db.*.private_ip
+}
