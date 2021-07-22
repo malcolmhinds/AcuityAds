c811c2adda1df86d072cf42c50993f5b52aef40e
malcolmhinds <40967754+malcolmhinds@users.noreply.github.com>
2021-07-21 21:40:53 -0400

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -2,15 +2,8 @@ provider "aws" {
   region = "ca-central-1"
 }
 
-resource "aws_db_instance" "myRDS" {
-  name = "myDB"
-  identifier = "my-first-rds"
-  instance_class = "db.t2.micro"
-  engine = "mariadb"
-  engine_version = "10.2.21"
-  username = "bob"
-  password = "password123"
-  port = 3306
-  allocated_storage = 20
-  skip_final_snapshot = true
+module "db" {
+  source= "../Challenge3/db"
+  servce_names = ["mariadb","mysql","MSsql"]
+  
 }
